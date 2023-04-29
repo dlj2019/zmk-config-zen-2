834cd7cbdf7d7733258b4436b69a756888cf229a
dlj2019 <30886694+dlj2019@users.noreply.github.com>
2023-04-29 17:35:43 -0400

    Update keycodes.h

--- config/keycodes.h
+++ config/keycodes.h
@@ -13,3 +13,17 @@
 #define _M__DN_L_    &mmv MOVE(-MOUSESPEED, MOUSESPEED)
 #define _M__LEFT_    &mmv MOVE_HOR(-MOUSESPEED)
 #define _M__UP_L_    &mmv MOVE(-MOUSESPEED, -MOUSESPEED)
+
+#define _M_LCLCK_    &mkp LCLK 
+#define _M_RCLCK_    &mkp RCLK
+#define _M_MCLCK_    &mkp MCLK  
+#define _M__BTN4_    &mkp MB4  
+#define _M__BTN5_    &mkp MB5  
+#define _M__BTN6_    &mkp MB6  
+#define _M__BTN7_    &mkp MB7  
+#define _M__BTN8_    &mkp MB8
+
+#define _SCRL_UP_    &mwh SCROLL_VERT(10) 
+#define _SCRL_DN_    &mwh SCROLL_VERT(-10) 
+#define _SCRL_LF_    &mwh SCROLL_HOR(-10) 
+#define _SCRL_RT_    &mwh SCROLL_HOR(10) 
